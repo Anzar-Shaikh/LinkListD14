3d16a6c22089719cb7eef05d719fdf6416d44908
anzar shaikh <anzarshaikh373@gmail.com>
2021-07-08 12:14:46 +0530

    ability to insert 30 between 50 & 70 is done.

--- src/com/bl/programmes/LinkLists.java
+++ src/com/bl/programmes/LinkLists.java
@@ -13,11 +13,14 @@ public class LinkLists {
         }
         public static void main(String[] args) {
             LinkLists lists = new LinkLists();
-            lists.head = new ListNode("50");
-            lists.head.next = new ListNode(30);
-            lists.head.next.next = new ListNode(70);
-            lists.print();
 
+            lists.head = new ListNode(50);
+            ListNode secondNode = new ListNode(70);
+            lists.head.next = secondNode;
+            ListNode thirdNode = new ListNode(30);
+            lists.head.next = thirdNode;
+            thirdNode.next = secondNode;
+            lists.print();
         }
     }
 
