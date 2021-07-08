7216ed7aa1298361ac179d7cdd2d8a016fe3886b
anzar shaikh <anzarshaikh373@gmail.com>
2021-07-08 14:25:01 +0530

    use case to seach node is done.

--- src/com/bl/programmes/LinkLists.java
+++ src/com/bl/programmes/LinkLists.java
@@ -3,11 +3,11 @@ package com.bl.programmes;
 public class LinkLists {
     public ListNode head;
 
-    public static class ListNode<K>{
-        public K data;
+    public static class ListNode{
+        public int data;
         public ListNode next;
 
-        public ListNode(K data) {
+        public ListNode(int data) {
             this.data = data;
             this.next = null;
         }
@@ -15,29 +15,43 @@ public class LinkLists {
             LinkLists lists = new LinkLists();
 
             lists.head = new ListNode(50);
+//            lists.head.next = new ListNode(70);
+//            lists.head.next.next = new ListNode(30);
             ListNode secondNode = new ListNode(70);
             ListNode thirdNode = new ListNode(30);
             lists.head.next = secondNode;
             secondNode.next = thirdNode;
             System.out.println("before roving first node in data.");
             lists.print();
-            lists.pop(secondNode, lists);
+//            lists.pop(secondNode, lists);
             lists.print();
-
+            search(lists, 30);
         }
-    }
+        private static void search(LinkLists lists ,int data) {
+            ListNode currentNode = lists.head , previousNode = null;
+//
+//            if (currentNode != null && currentNode.data == data){
+//                System.out.println(data);
+//            }
+            while ( currentNode != null && currentNode.data != data){
 
+                currentNode = currentNode.next;
+            }
+            System.out.println("node is found with data 30 :: "+currentNode.data);
+        }
+
+    }
     private void pop(ListNode node, LinkLists lists) {
+        ListNode courser = head;
         System.out.println("after removing head node from link list.");
-        lists.head = node;
-    }
 
+    }
     private void print() {
         ListNode pointer = head;
-
         while (pointer != null){
             System.out.print(pointer.data+ "->");
             pointer = pointer.next;
+
         }
         System.out.println("null");
     }
